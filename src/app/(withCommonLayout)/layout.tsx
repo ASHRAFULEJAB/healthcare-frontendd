d97fb4e81d74faf83a57ac94df7d50bbe7b2894e
Ashraful Islam <19103189@iubat.edu>
2024-05-29 03:20:24 +0600

    doctor specialist section added

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,6 +1,11 @@
 import Footer from "@/components/Shared/Footer";
 import Navbar from "@/components/Shared/Navbar";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Home || Ashh Clinic",
+  description: "Ashh clinic home page",
+};
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,19 +1,19 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 // import HowItWorks from "@/components/UI/HomePage/HowItWorks/HowItWorks";
-// import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
 // import Stats from "@/components/UI/HomePage/Stats/Stats";
-// import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 // import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
-      {/* <Specialist />
+      <Specialist />
       <TopRatedDoctors />
-      <WhyUs />
-      <HowItWorks />
-      <Stats /> */}
+      {/* <WhyUs /> */}
+      {/* <HowItWorks /> */}
+      {/* <Stats /> */}
     </>
   );
 };
