fab2b01ae3c6b525847f59b7b467ebff4108b584
Ashraful Islam <19103189@iubat.edu>
2024-05-28 19:36:04 +0600

    inital setup with navbar and footer

---
+++ src/app/(withCommonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Shared/Footer";
+import Navbar from "@/components/Shared/Navbar";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen">{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default CommonLayout;
