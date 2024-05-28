98bee4b354c806302ad4e6fa4f6a2d7e703ab797
Ashraful Islam <19103189@iubat.edu>
2024-05-28 23:03:14 +0600

    hero section added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@mui/material";
+import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+// import HowItWorks from "@/components/UI/HomePage/HowItWorks/HowItWorks";
+// import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
+// import Stats from "@/components/UI/HomePage/Stats/Stats";
+// import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+// import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div>
-      <h1>HEY</h1>
-      <Button>Ki obosta</Button>
-    </div>
+    <>
+      <HeroSection />
+      {/* <Specialist />
+      <TopRatedDoctors />
+      <WhyUs />
+      <HowItWorks />
+      <Stats /> */}
+    </>
   );
-}
+};
+
+export default HomePage;
